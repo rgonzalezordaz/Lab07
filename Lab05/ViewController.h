35f08e7107c04a9dbe96e83f482c1f9653953279
Ricardo Gonzalez <Ricardo@macbook-pro-de-ricardo.local>
2016-06-28 21:50:45 -0500

    Initial Commit

---
+++ Lab05/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab05
+//
+//  Created by Ricardo Gonzalez on 28/06/16.
+//  Copyright © 2016 RGO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
